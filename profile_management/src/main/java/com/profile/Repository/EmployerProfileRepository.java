110780ff3d8096fe941de703aba642adb1ed8519
Shreyas Satish Rane <145131378+ShreyasRane2@users.noreply.github.com>
2026-02-13 23:58:27 +0530

    Delete profile_management directory

--- profile_management/src/main/java/com/profile/Repository/EmployerProfileRepository.java
+++
@@ -1,9 +0,0 @@
-package com.profile.Repository;
-
-import com.profile.entity.EmployerProfile;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface EmployerProfileRepository extends JpaRepository<EmployerProfile, Long> {
-    Optional<EmployerProfile> findByEmail(String email);
-}
--- profile_management/src/main/java/com/profile/Repository/JobSeekerProfileRepository.java
+++
@@ -1,9 +0,0 @@
-package com.profile.Repository;
-
-import com.profile.entity.JobSeekerProfile;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface JobSeekerProfileRepository extends JpaRepository<JobSeekerProfile, Long> {
-    Optional<JobSeekerProfile> findByEmail(String email);
-}
